c0dbf1a641d42023bcf20ee1bce132a77a2e6ab5
Zabir Haider Khan <xabirkhan@gmail.com>
2020-12-10 21:35:07 +0100

    add nav

--- src/_nav.js
+++ src/_nav.js
@@ -6,134 +6,141 @@ export default {
       icon: 'Home',
     },
     {
-      name: 'UI Elements',
+      name: 'Category',
       icon: 'Layers',
       children: [
         {
-          name: 'Buttons',
-          url: '/elements/buttons',
+          name: 'Catagories',
+          url: '/catagory/list',
         },
         {
-          name: 'Grid',
-          url: '/elements/grid',
+          name: 'Add new catagory',
+          url: '/catagory/add',
         },
-        {
-          name: 'Alerts',
-          url: '/elements/alerts',
-        },
-        {
-          name: 'Typography',
-          url: '/elements/typography',
-        },
-        {
-          name: 'Cards',
-          url: '/elements/cards',
-        },
-        {
-          name: 'Tabs',
-          url: '/elements/tabs',
-        },
-        {
-          name: 'Tables',
-          url: '/elements/tables',
-        },
-        {
-          name: 'Breadcrumbs',
-          url: '/elements/breadcrumbs',
-        },
-        {
-          name: 'Forms',
-          url: '/elements/forms',
-        },
-        {
-          name: 'Modals',
-          url: '/elements/modals',
-        },
-        {
-          name: 'Loaders',
-          url: '/elements/loaders',
-        },
-        {
-          name: 'Avatars',
-          url: '/elements/avatars',
-        },
-        {
-          name: 'Progress Bars',
-          url: '/elements/progressbars',
-        },
-        {
-          name: 'Pagination',
-          url: '/elements/pagination',
-        },
+        // {
+        //   name: 'Alerts',
+        //   url: '/elements/alerts',
+        // },
+        // {
+        //   name: 'Typography',
+        //   url: '/elements/typography',
+        // },
+        // {
+        //   name: 'Cards',
+        //   url: '/elements/cards',
+        // },
+        // {
+        //   name: 'Tabs',
+        //   url: '/elements/tabs',
+        // },
+        // {
+        //   name: 'Tables',
+        //   url: '/elements/tables',
+        // },
+        // {
+        //   name: 'Breadcrumbs',
+        //   url: '/elements/breadcrumbs',
+        // },
+        // {
+        //   name: 'Forms',
+        //   url: '/elements/forms',
+        // },
+        // {
+        //   name: 'Modals',
+        //   url: '/elements/modals',
+        // },
+        // {
+        //   name: 'Loaders',
+        //   url: '/elements/loaders',
+        // },
+        // {
+        //   name: 'Avatars',
+        //   url: '/elements/avatars',
+        // },
+        // {
+        //   name: 'Progress Bars',
+        //   url: '/elements/progressbars',
+        // },
+        // {
+        //   name: 'Pagination',
+        //   url: '/elements/pagination',
+        // },
       ],
     },
     {
-      name: 'Pages',
+      name: 'Post',
       icon: 'File',
       children: [
         {
-          name: 'Blank',
-          url: '/pages/blank',
+          name: 'Posts',
+          url: '/post/list',
         },
         {
-          name: 'Sub Navigation',
-          url: '/pages/subnav',
+          name: 'Add new post',
+          url: '/post/add',
         },
-        {
-          name: '404',
-          url: '/pages/404',
-        },
+        // {
+        //   name: 'Blank',
+        //   url: '/pages/blank',
+        // },
+        // {
+        //   name: 'Sub Navigation',
+        //   url: '/pages/subnav',
+        // },
+        // {
+        //   name: '404',
+        //   url: '/pages/404',
+        // },
       ],
     },
-    {
-      name: 'Apps',
-      icon: 'Cloud',
-      children: [
-        {
-          name: 'Analytics',
-          url: '/apps/analytics',
-        },
-        {
-          name: 'Invoice',
-          url: '/apps/invoice',
-        },
-        {
-          name: 'Activity Feed',
-          url: '/apps/feed',
-        },
-        {
-          name: 'CMS',
-          url: '/apps/cms',
-        },
-      ],
-    },
+    // {
+    //   name: 'Apps',
+    //   icon: 'Cloud',
+    //   children: [
+    //     {
+    //       name: 'Analytics',
+    //       url: '/apps/analytics',
+    //     },
+    //     {
+    //       name: 'Invoice',
+    //       url: '/apps/invoice',
+    //     },
+    //     {
+    //       name: 'Activity Feed',
+    //       url: '/apps/feed',
+    //     },
+    //     {
+    //       name: 'CMS',
+    //       url: '/apps/cms',
+    //     },
+    //   ],
+    // },
     {
       divider: true,
     },
-    {
-      name: 'Widgets',
-      url: '/widgets',
-      icon: 'Package',
-      badge: {
-        text: 'NEW',
-      },
-    },
+    // {
+    //   name: 'Widgets',
+    //   url: '/widgets',
+    //   icon: 'Package',
+    //   badge: {
+    //     text: 'NEW',
+    //   },
+    // },
   ],
   bottom: [
+    // {
+    //   name: 'Get Vibe',
+    //   url: 'https://github.com/NiceDash/Vibe',
+    //   icon: 'GitHub',
+    //   external: true,
+    //   target: '_blank',
+    // },
     {
-      name: 'Get Vibe',
-      url: 'https://github.com/NiceDash/Vibe',
-      icon: 'GitHub',
-      external: true,
-      target: '_blank',
-    },
-    {
-      name: 'Account',
-      url: '/dashboard',
+      name: 'Logout',
+      url: '/logout',
       icon: 'User',
       badge: {
         variant: 'success',
-        text: '3',
       },
     },
   ],
